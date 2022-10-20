09dba15e5aefdd921d6e4d12ea4450b46322eb61
Eric Lagergren <elagergren@spideroak-inc.com>
2022-10-20 12:05:53 -0700

    add Value methods
    These are useful when T or E are used for resources that must be closed.
    For example:
    
        type T struct {
    	db   *sql.DB
    	stmt lazy.T[*db.Stmt]
        }
    
        func (t *T) doThing() error {
    	stmt, err := t.stmt.Get(func() (*sql.Stmt, error) {
    	    return t.db.Prepare(...)
    	})
    	if err != nil { ...  }
    	...
        }
    
        func (t *T) Close() error{
    	if s, ok := t.stmt.Value(); ok {
    	    s.Close()
    	}
    	return t.db.Close()
        }
    
    Signed-off-by: Eric Lagergren <elagergren@spideroak-inc.com>

--- lazy.go
+++ lazy.go
@@ -19,11 +19,15 @@
 // I'm lazy and we have generics, so why not ab^H^Huse them?
 package lazy
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 type T[V any] struct {
-	once sync.Once
-	v    V
+	done uint32     // set to 1 after Get is called
+	m    sync.Mutex // guards v while writing
+	v    V          // underlying value
 }
 
 // Get calls fn if and only if it is being called for the first
@@ -31,17 +35,37 @@ type T[V any] struct {
 //
 // Subsequent invocations of Get return the same result.
 //
-// It has the same semantics as sync.Once because it's a wrapper
-// around sync.Once.
+// It has the same semantics as [sync.Once].
 func (t *T[V]) Get(fn func() V) V {
-	t.once.Do(func() { t.v = fn() })
+	if atomic.LoadUint32(&t.done) == 0 {
+		t.m.Lock()
+		defer t.m.Unlock()
+
+		if t.done == 0 {
+			defer atomic.StoreUint32(&t.done, 1)
+			t.v = fn()
+		}
+	}
 	return t.v
 }
 
+// Value returns the cached value, if set.
+func (t *T[V]) Value() (V, bool) {
+	if atomic.LoadUint32(&t.done) != 0 {
+		return t.v, true
+	}
+
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	return t.v, t.done != 0
+}
+
 type E[V any] struct {
-	once sync.Once
-	v    V
-	err  error
+	done uint32     // set to 1 after Get is called
+	m    sync.Mutex // guards v, err while writing
+	v    V          // underlying value
+	err  error      // underlying value
 }
 
 // Get calls fn if and only if it is being called for the first
@@ -49,13 +73,30 @@ type E[V any] struct {
 //
 // Subsequent invocations of Get return the same result.
 //
-// It has the same semantics as sync.Once because it's a wrapper
-// around sync.Once.
+// It has the same semantics as [sync.Once].
 func (t *E[V]) Get(fn func() (V, error)) (V, error) {
-	t.once.Do(func() {
-		t.v, t.err = fn()
-	})
+	if atomic.LoadUint32(&t.done) == 0 {
+		t.m.Lock()
+		defer t.m.Unlock()
+
+		if t.done == 0 {
+			defer atomic.StoreUint32(&t.done, 1)
+			t.v, t.err = fn()
+		}
+	}
 	return t.v, t.err
+}
+
+// Value returns the cached value, if set.
+func (t *E[V]) Value() (V, bool) {
+	if atomic.LoadUint32(&t.done) != 0 {
+		return t.v, true
+	}
+
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	return t.v, t.done != 0
 }
 
 // Must returns panics if err != nil and returns t otherwise.
